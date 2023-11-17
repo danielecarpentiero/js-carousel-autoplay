014f6653fd342fc48fca4a7d133ad4584e29bf29
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-17 17:22:25 +0100

    feat: milestone principale fatta

--- js/main.mjs
+++ js/main.mjs
@@ -1,66 +1,88 @@
-const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const items = document.querySelector(".items");
-const thumbnailsContainer = document.querySelector('.tb-container');
+const thumbnailsContainer = document.querySelector(".tb-container");
 let currentImage = 0;
 let itemsAll;
 
-for (let i = 0; i < images.length; i++) {
+function createItem(index) {
   const item = document.createElement("div");
-  item.classList.add('item');
+  item.classList.add("item");
 
-  const img = document.createElement('img');
-  img.src = `img/${images[i]}`;
-  img.alt = `01 ${i}`;
+  const img = document.createElement("img");
+  img.src = `img/${images[index]}`;
+  img.alt = `01 ${index}`;
 
   item.append(img);
-  items.append(item);
+  return item;
+}
 
-  const thumbnail = document.createElement('div');
-  thumbnail.classList.add('tb');
+function createThumbnail(index) {
+  const thumbnail = document.createElement("div");
+  thumbnail.classList.add("tb");
 
-  const thumbnailImg = document.createElement('img');
-  thumbnailImg.src = `img/${images[i]}`;
-  thumbnailImg.alt = `thumbnail ${i}`;
+  const thumbnailImg = document.createElement("img");
+  thumbnailImg.src = `img/${images[index]}`;
+  thumbnailImg.alt = `thumbnail ${index}`;
 
   thumbnail.append(thumbnailImg);
+  return thumbnail;
+}
+
+function setActive(index) {
+  itemsAll[currentImage].classList.remove("active");
+  thumbnailsContainer.querySelector(".tb.active").classList.remove("active");
+  currentImage = index;
+  itemsAll[currentImage].classList.add("active");
+  thumbnailsContainer
+    .querySelectorAll(".tb")
+    [currentImage].classList.add("active");
+}
+
+function showPrev() {
+  let newIndex = currentImage - 1;
+  if (newIndex < 0) {
+    newIndex = itemsAll.length - 1;
+  }
+  setActive(newIndex);
+}
+
+function showNext() {
+  let newIndex = currentImage + 1;
+  if (newIndex >= itemsAll.length) {
+    newIndex = 0;
+  }
+  setActive(newIndex);
+}
+
+for (let i = 0; i < images.length; i++) {
+  const item = createItem(i);
+  const thumbnail = createThumbnail(i);
+
+  items.append(item);
   thumbnailsContainer.append(thumbnail);
 
   if (currentImage === i) {
-    item.classList.add('active');
-    thumbnail.classList.add('active');
+    item.classList.add("active");
+    thumbnail.classList.add("active");
   }
 
-  /* Aggiungi evento di click per le miniature */
-  thumbnail.addEventListener('click', function () {
-    itemsAll[currentImage].classList.remove('active');
-    thumbnailsContainer.querySelector('.tb.active').classList.remove('active');
-    currentImage = i;
-    itemsAll[currentImage].classList.add('active');
-    thumbnailsContainer.querySelectorAll('.tb')[currentImage].classList.add('active');
+  thumbnail.addEventListener("click", function () {
+    setActive(i);
   });
 }
 
-/* seleziono tutte le classi che contengono le immagini dopo averle create */
-itemsAll = document.querySelectorAll('.item');
+itemsAll = document.querySelectorAll(".item");
 
-/* al click su prev visualizza l'immagine precedente */
-prev.addEventListener('click', function () {
-  console.log("ho cliccato su prev");
-  itemsAll[currentImage].classList.remove('active');
-  thumbnailsContainer.querySelector('.tb.active').classList.remove('active');
-  currentImage = (currentImage - 1 + itemsAll.length) % itemsAll.length;
-  itemsAll[currentImage].classList.add('active');
-  thumbnailsContainer.querySelectorAll('.tb')[currentImage].classList.add('active');
+prev.addEventListener("click", function () {
+  showPrev();
 });
 
-/* al click su next visualizza l'immagine successiva */
-next.addEventListener('click', function () {
-  console.log("ho cliccato su next");
-  itemsAll[currentImage].classList.remove('active');
-  thumbnailsContainer.querySelector('.tb.active').classList.remove('active');
-  currentImage = (currentImage + 1) % itemsAll.length;
-  itemsAll[currentImage].classList.add('active');
-  thumbnailsContainer.querySelectorAll('.tb')[currentImage].classList.add('active');
+next.addEventListener("click", function () {
+  showNext();
 });
+
+/* aggiungere l'autoplay */
+
+setInterval(showNext, 3000);
